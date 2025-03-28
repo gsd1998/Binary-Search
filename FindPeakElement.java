486c2d54e4d069a6ab68f6d6c52fe2c1d2c35ec0
Giri Shanker D <43494225+gsd1998@users.noreply.github.com>
2025-03-29 01:57:28 +0530

    Add files via upload

---
+++ FindPeakElement.java
@@ -0,0 +1,36 @@
+package interviewQA.BinarySearchQA;
+
+public class FindPeakElement {
+
+    public static void main(String[] args){
+        int[] nums = {1,2,1,3,5,6,4};
+        System.out.println(findPeakElement(nums));//5
+    }
+
+    public static int findPeakElement(int[] arr) {
+        int n = arr.length;
+        if(n == 1){
+            return 0;
+        }
+        if(arr[0] > arr[1]){
+            return 0;
+        }
+        if(arr[n-1] > arr[n-2]){
+            return n-1;
+        }
+        int low = 1;
+        int high = n-2;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid] > arr[mid-1]){
+                low = mid + 1;
+            }else if(arr[mid] < arr[mid-1]){
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+}
